5ab77c7bd84920adac7081001f93674f6cc95bb5
xDrunk1 <yousef245737@bue.edu.eg>
2025-11-11 02:19:29 +0200

    Enemy Attempt

---
+++ My project/Assets/Scripts/GhostEnemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostEnemy : EnemyController
+{
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.flipX = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (sr.flipX == true)
+        {
+            this.GetComponent<Rigidbody2D>().velocity =
+                new Vector2(-maxSpeed, this.GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else
+        {
+            this.GetComponent<Rigidbody2D>().velocity =
+                new Vector2(maxSpeed, this.GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
